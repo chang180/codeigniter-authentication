bce45db1dae8093d2e37c8d4a34312be6f20e516
張建文 <chang180@gmail.com>
2021-04-16 21:30:28 +0800

    register processing

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -4,6 +4,11 @@ namespace App\Controllers;
 
 class Auth extends BaseController
 {
+    public function __construct()
+    {
+        helper(['url', 'form']);
+    }
+
     public function index()
     {
         return view('auth/login');
@@ -12,5 +17,69 @@ class Auth extends BaseController
     public function register()
     {
         return view('auth/register');
+    }
+    public function save()
+    {
+        // Validate requests
+        // $validation = $this->validate([
+        //     'name' => 'required',
+        //     'email' => 'required|valid_email|is_unique[users.email]',
+        //     'password' => 'required|min_length[5]|max_length[12]',
+        //     'cpassword' => 'required|min_length[5]|max_length[12]|matches[password]'
+        // ]);
+        $validation = $this->validate([
+            'name' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Your full name is required'
+                ]
+            ],
+            'email' => [
+                'rules' => 'required|valid_email|is_unique[users.email]',
+                'errors' => [
+                    'required' => 'Email is required',
+                    'valid_email' => 'You must enter a valid email',
+                    'is_unique' => 'Email already Taken'
+                ]
+            ],
+            'password' => [
+                'rules' => 'required|min_length[5]|max_length[12]',
+                'errors' => [
+                    'required' => 'Password is required',
+                    'min_length' => 'Password must have at least 5 characters in length',
+                    'max_length' => 'Password must not have characters more than 12 in length'
+                ]
+            ],
+            'cpassword' => [
+                'rules' => 'required|min_length[5]|max_length[12]|matches[password]',
+                'errors' => [
+                    'required' => 'Confirm password is required',
+                    'min_length' => 'Confirm password must have at least 5 characters in length',
+                    'max_length' => 'Confirm password must not have characters more than 12 in length',
+                    'matches' => 'Confirm password not matches to password'
+                ]
+            ]
+        ]);
+
+        if (!$validation) {
+            return view('auth/register', ['validation' => $this->validator]);
+        } else {
+            // Register use into DB
+            $name = $this->request->getPost('name');
+            $email = $this->request->getPost('email');
+            $password = $this->request->getPost('password');
+
+            $values = [
+                'name' => $name,
+                'email' => $email,
+                'password' => $password,
+            ];
+
+            $usersModel = new \App\Models\UsersModel();
+            $query = $usersModel->insert($values);
+            if(!$query){
+                return redirect()->back()->with('fail','Something went wrong');
+            }
+        }
     }
 }
--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -15,28 +15,33 @@
             <div class="col-md-4 offset-md-4">
                 <h4>Sign In</h4>
                 <hr>
-                <form action="" class="form-control">
+                <form action="<?= base_url('auth/save'); ?>" method="post" class="form-control">
+                    <?= csrf_field(); ?>
                     <label for="" class="form-label">Name</label>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" name="name" placeholder="Enter full name">
+                        <input type="text" class="form-control" name="name" placeholder="Enter full name" value="<?= set_value('name');?>">
                     </div>
+                    <div class="text-danger mb-3"><?= isset($validation) ? display_errors($validation, 'name') : ''; ?></div>
                     <label for="" class="form-label">Email</label>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" name="email" placeholder="Enter your email">
+                        <input type="text" class="form-control" name="email" placeholder="Enter your email" value="<?= set_value('email');?>">
                     </div>
+                    <div class="text-danger mb-3"><?= isset($validation) ? display_errors($validation, 'email') : ''; ?></div>
                     <label for="" class="form-label">Password</label>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" name="password" placeholder="Enter password">
+                        <input type="text" class="form-control" name="password" placeholder="Enter password" value="<?= set_value('password');?>">
                     </div>
+                    <div class="text-danger mb-3"><?= isset($validation) ? display_errors($validation, 'password') : ''; ?></div>
                     <label for="" class="form-label">Confirm Password</label>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" name="cpassword" placeholder="Confirm password">
+                        <input type="text" class="form-control" name="cpassword" placeholder="Confirm password" value="<?= set_value('cpassword');?>">
                     </div>
+                    <div class="text-danger mb-3"><?= isset($validation) ? display_errors($validation, 'cpassword') : ''; ?></div>
                     <div class="d-grid mb-3">
                         <button class="btn btn-primary">Sign Up</button>
                     </div>
                     <br>
-                    <a href="<?=site_url('auth');?>" class="text-decoration-none">I already have an account, login now</a>
+                    <a href="<?= site_url('auth'); ?>" class="text-decoration-none">I already have an account, login now</a>
                 </form>
             </div>
         </div>
